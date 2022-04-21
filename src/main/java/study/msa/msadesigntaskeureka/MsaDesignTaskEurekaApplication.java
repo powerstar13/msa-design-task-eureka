6799273c7e3c4f1e5b5237085fa067fbcf510809
홍준성 <powerstar13@hanmail.net>
2022-04-22 00:27:07 +0900

    Application > Eureka Server 역할 수행하도록 어노테이션 추가

--- src/main/java/study/msa/msadesigntaskeureka/MsaDesignTaskEurekaApplication.java
+++ src/main/java/study/msa/msadesigntaskeureka/MsaDesignTaskEurekaApplication.java
@@ -2,8 +2,10 @@ package study.msa.msadesigntaskeureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer // 유레카 서버의 역할을 수행한다.
 public class MsaDesignTaskEurekaApplication {
 
     public static void main(String[] args) {
